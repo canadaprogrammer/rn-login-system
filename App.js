5861e2502c1eebac5b848086d77a39b54f2388eb
Jin Park <jinpark1504@gmail.com>
2022-06-18 21:38:22 -0700

    sign up screen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import Login from './screens/Login';
+import Signup from './screens/Signup';
 
 export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <Login />
+      <Signup />
     </>
   );
 }
